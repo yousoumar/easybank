0b5253ea90c640a933e3847088f710a76cf3dda5
yousoumar <oumaiyr@gmail.com>
2021-08-12 11:53:30 +0200

    optimizes user experience

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,27 @@ module.exports = {
                     },
                 ]
             },
+
+            {
+                test: /\.css$/i,
+                use : [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", 
+                    {
+                        loader : "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                              plugins: [
+                                [
+                                  "postcss-preset-env",
+                                ],
+                              ],
+                            },
+                        }
+                    },
+                    
+                ]
+            },
        
             {
                 test: /\.js$/,
