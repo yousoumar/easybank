c99165fabe73c6083217b7eb08304f493e347d94
yousoumar <oumaiyr@gmail.com>
2021-08-14 11:45:43 +0200

    adds a loader and animations

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,28 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', (e)=>{
     header.classList.toggle('clicked')
 });
+
+/* -------------------- IntersectionObserver && loader ----------------------------- */
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .3
+  }
+  
+  window.addEventListener('load', ()=>{
+    setTimeout(()=>{
+      document.body.classList.add("loaded");
+      const observer = new IntersectionObserver(handleIntersect, options);
+      document.querySelectorAll('.will-be-revealed').forEach((r)=>{observer.observe(r)});
+    },1000);
+  });
+  
+  
+  function handleIntersect(entries, observer){
+    entries.forEach(function(entry){
+      if (entry.isIntersecting){
+        entry.target.classList.add('will-be-revealed-revealed'); 
+      }
+      
+    });
+  }
